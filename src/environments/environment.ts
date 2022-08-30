4beea981f68a99fe77e94eb4ab8aa45c12741b58
Ahmed Hamza <hdamnite@gmail.com>
2022-08-30 10:54:11 +0530

    made prod in environment true

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   apiEndpoint: 'https://api.stackexchange.com/2.3/search/advanced',
   //Filter for Api with page,page_size,body,total,up_vote_count
   apiFilter:'&site=stackoverflow&filter=!*MZqiH2P50zSR0*b',
