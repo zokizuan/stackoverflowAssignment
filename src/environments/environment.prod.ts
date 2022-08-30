fbce2e45251ef2f6153d97f57dd4ca1b19efdde0
Ahmed Hamza <hdamnite@gmail.com>
2022-08-30 11:40:16 +0530

    added api endpoint to prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiEndpoint: 'https://api.stackexchange.com/2.3/search/advanced',
+  //Filter for Api with page,page_size,body,total,up_vote_count
+  apiFilter:'&site=stackoverflow&filter=!*MZqiH2P50zSR0*b',
 };
